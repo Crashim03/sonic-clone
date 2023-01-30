a510a894047b070823da466f1d7f94aa8d0239ac
Alexandre Duarte <alexteloduarte@gmail.com>
2023-01-30 16:36:30 +0000

    Breaking animation fixed

--- Assets/Code/Player/Player.cs
+++ Assets/Code/Player/Player.cs
@@ -24,6 +24,7 @@ public class Player : MonoBehaviour
     public float _lastDirection = 1;
     public bool _isSpindashing = false;
     public bool _isSuperSpeeding = false;
+    public bool _isBreaking = false;
     public SpriteRenderer _spriteRenderer;
 
     private void Start()
@@ -37,6 +38,7 @@ public class Player : MonoBehaviour
         _animator.SetFloat("Speed", Math.Abs(_speed));
         _animator.SetBool("Spindash", _isSpindashing);
         _animator.SetBool("SuperSpeed", _isSuperSpeeding);
+        _animator.SetBool("Breaking", _isBreaking);
 
         if (_speed > 0)
         {
@@ -84,19 +86,28 @@ public class Player : MonoBehaviour
 
     public void Move(float direction, float acceleration, float deceleration, float max_speed, bool canBreak)
     {
+        if (!canBreak)
+        {
+            _isBreaking = false;
+        }
+
         if (direction > 0)
         {
             if (_speed < 0)
             {
                 if (canBreak)
                 {
-                    _speed += deceleration * 10;
-                    Debug.Log("Breaking");
+                    _speed += deceleration * 6;
+                    _isBreaking = true;
                 }
                 else
                     _speed += deceleration * 2;
 
-                if (_speed > 0) { _speed = 0; }
+                if (_speed > 0)
+                {
+                    _speed = 0;
+                    _isBreaking = false;
+                }
             }
             else
             {
@@ -117,13 +128,17 @@ public class Player : MonoBehaviour
             {
                 if (canBreak)
                 {
-                    _speed -= deceleration * 10;
-                    Debug.Log("Breaking");
+                    _speed -= deceleration * 6;
+                    _isBreaking = true;
                 }
                 else
                     _speed -= deceleration * 2;
 
-                if (_speed < 0) { _speed = 0; }
+                if (_speed < 0)
+                {
+                    _speed = 0;
+                    _isBreaking = false;
+                }
             }
             else
             {
@@ -140,17 +155,27 @@ public class Player : MonoBehaviour
         }
         else
         {
+            _isBreaking = false;
+
             if (_speed > 0)
             {
                 _speed -= deceleration;
 
-                if (_speed < 0) { _speed = 0; }
+                if (_speed < 0)
+                {
+                    _speed = 0;
+                    _isBreaking = false;
+                }
             }
             else if (_speed < 0)
             {
                 _speed += deceleration;
 
-                if (_speed > 0) { _speed = 0; }
+                if (_speed > 0)
+                {
+                    _speed = 0;
+                    _isBreaking = false;
+                }
             }
         }
 
